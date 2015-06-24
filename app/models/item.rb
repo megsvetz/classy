8416bdbdb0632d54d28f466bf17f8cab3eeb2373
Meg Svetz <megsvetz@gmail.com>
2015-06-24 15:05:37 -0600

    updated model specs

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,11 @@
 class Item < ActiveRecord::Base
   belongs_to :cat
   validates_presence_of :name
+
+  def self.by_name
+    order(:name)
+  end
+  def self.by_price
+    order(:price)
+  end
 end
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,5 +1,26 @@
 require 'rails_helper'
 
 RSpec.describe Item, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  context "class methods" do
+    before(:each) do
+      @item_1 = Item.create(name:"Old Couch", price:50.00, description: "Well loved, but great for spooning!", cat_id: 6)
+      @item_2 = Item.create(name:"Fridge", price:157, description: "French door stainless steel LG fridge, 2006...in working condition.", cat_id: 4)
+      @item_3 = Item.create(name:"Dining Table and Chair Set", price:79, description: "Oak square dining table and chair set, includes 4 chairs. Pick-up only.", cat_id: 2)
+      @item_4 = Item.create(name:"Bike", price:125, description: "2002 Cannondale bike.", cat_id: 3)
+    end
+
+    describe ".by_name" do
+      it "alphabetizes by name" do
+        expect(Item.all.by_name.first).to eq(@item_4)
+      end
+    end
+
+    describe ".by_price" do
+      it "orders by price" do
+        expect(Item.all.by_price.first).to eq(@item_1)
+      end
+    end
+  end
+
 end
