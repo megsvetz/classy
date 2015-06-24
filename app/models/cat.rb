6ae65af1f853db1764be9ea5621709737eedc59d
reecebisel <reecebisel@gmail.com>
2015-06-24 15:12:14 -0600

    class model methods test in aplhbetical order for cats

--- app/models/cat.rb
+++ app/models/cat.rb
@@ -2,5 +2,7 @@ class Cat < ActiveRecord::Base
   has_many :items
   validates_presence_of :name
 
-  
+  def self.by_name
+    order(:name)  
+  end
 end
--- spec/models/cat_spec.rb
+++ spec/models/cat_spec.rb
@@ -1,5 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe Cat, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  context "class methods" do
+    before(:each) do
+      #don't forget to make them instance variables with an @ sign
+      @cat_1 = Cat.create(name: "Cars")
+      @cat_2 = Cat.create(name: "Music")
+      @cat_3 = Cat.create(name: "Zebras")
+      @cat_4 = Cat.create(name: "Zebras gold")
+    end
+    describe ".by_name" do
+      it "orders alphabetically" do
+        expect(Cat.all.by_name.first).to eq(@cat_1)
+      end
+    end #describe .by_name endtag
+  end #context class methods endtag
 end
