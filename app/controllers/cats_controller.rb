7d1830cf3bd4b3148c9785f32888978f3d492965
reecebisel <reecebisel@gmail.com>
2015-06-24 12:06:03 -0600

    added crud actions to cats controller

--- app/controllers/cats_controller.rb
+++ app/controllers/cats_controller.rb
@@ -1,23 +1,55 @@
 class CatsController < ApplicationController
+  before_action find_cat, only: [:update, :edit, :destroy, :show]
+
   def index
-  end
+    @cats = Cat.all
+  end #index endtag
 
   def show
   end
 
   def new
+    @cat = Cat.new
   end
 
   def create
-  end
+    if @cat.save(cat_params)
+      flash[:notice]= "Category created successfully!"
+      redirect_to(cats_path)
+    else
+      flash[:error]= "Category creation failed!"
+      render :new
+    end
+  end # Create endtag
 
   def edit
   end
 
   def update
-  end
+    if @cat.update(cat_params)
+      flash[:notice]= "Successfully updated!"
+      redirect_to(cats_path)
+    else
+      flash[:error]= "Update Failed!"
+      render :edit
+    end
+  end #update endtag
 
   def destroy
+    if @cat.destroy
+      flash[:alert]="Category deleted!"
+    else
+      flash[:error]="Category deletion failed!"
+  end
+
+  private
+
+  def cat_params
+    params.require(:cat).permit(:name)
+  end
+
+  def find_cat
+    @cat = Cat.find_by(:id params[:id])
   end
 
 end
