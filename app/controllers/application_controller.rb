0a5af642490314a0460577b5db7ed3ef1f21b388
Meg Svetz <megsvetz@gmail.com>
2015-06-29 09:28:30 -0600

    fixed My Listings page in navbar

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,11 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   before_action :authenticate_user!
+
+  def application
+    @user = current_user
+    unless @user
+      render(text: "User not found.", status: :not_found)
+    end
+  end
 end
